9794c3da7f5762314191d97e9e2d021abcc8d918
Md Zainul Qureshi <mmohdzainulaabdeen@gmail.com>
2026-06-17 22:19:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const cors = require("cors");
 
@@ -6,15 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const appointments = [];
-const contacts = [];
-
 app.get("/", (req, res) => {
   res.send("MedHelp Backend Running");
 });
 
 app.post("/appointment", (req, res) => {
-  appointments.push(req.body);
   res.json({
     success: true,
     message: "Appointment request received"
@@ -22,19 +19,10 @@ app.post("/appointment", (req, res) => {
 });
 
 app.post("/contact", (req, res) => {
-  contacts.push(req.body);
   res.json({
     success: true,
     message: "Contact message received"
   });
-});
-
-app.get("/appointments", (req, res) => {
-  res.json(appointments);
-});
-
-app.get("/contacts", (req, res) => {
-  res.json(contacts);
 });
 
 const PORT = process.env.PORT || 5000;
