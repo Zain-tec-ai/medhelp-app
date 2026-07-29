b3df2bf8d46f323543cba146d599bd93ddc9cebd
Md Zainul Qureshi <mmohdzainulaabdeen@gmail.com>
2026-07-29 23:22:51 +0530

    Create Appointment model with schema definition

--- backend/models/appointment.js
+++ backend/models/appointment.js
@@ -1,1 +1,39 @@
+const mongoose = require("mongoose");
 
+const AppointmentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+
+    phone: {
+      type: String,
+      required: true
+    },
+
+    department: {
+      type: String,
+      required: true
+    },
+
+    date: {
+      type: String,
+      required: true
+    },
+
+    reason: {
+      type: String
+    },
+
+    report: {
+      type: String
+    }
+  },
+  {
+    collection: "appointments",
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Appointment", AppointmentSchema);
