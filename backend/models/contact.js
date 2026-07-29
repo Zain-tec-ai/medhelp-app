75a896983dd3a9a090a5b15a8ada958e0ef91030
Md Zainul Qureshi <mmohdzainulaabdeen@gmail.com>
2026-07-29 23:23:37 +0530

    Update contact.js

--- backend/models/contact.js
+++ backend/models/contact.js
@@ -1,1 +1,23 @@
+const mongoose = require("mongoose");
 
+const ContactSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    message: {
+      type: String,
+      required: true,
+      trim: true
+    }
+  },
+  {
+    collection: "contacts",
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Contact", ContactSchema);
