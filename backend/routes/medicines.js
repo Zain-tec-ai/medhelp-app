e5567bf56e26eb3f6006baa92bfca9569b110742
Md Zainul Qureshi <mmohdzainulaabdeen@gmail.com>
2026-07-29 23:20:51 +0530

    Add Medicine schema for mongoose model

--- backend/routes/medicines.js
+++ backend/routes/medicines.js
@@ -1,1 +1,34 @@
+const mongoose = require("mongoose");
 
+const MedicineSchema = new mongoose.Schema(
+  {
+    drugCode: {
+      type: Number,
+      required: true,
+      unique: true
+    },
+
+    genericName: {
+      type: String,
+      required: true
+    },
+
+    unitSize: {
+      type: String
+    },
+
+    mrp: {
+      type: Number
+    },
+
+    groupName: {
+      type: String
+    }
+  },
+  {
+    collection: "medicine",
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Medicine", MedicineSchema);
